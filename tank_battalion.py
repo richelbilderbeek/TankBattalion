266ceeb45628fae773817876a2c942119f191a50
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-04 17:44:12 +0200

    Tanks moves

--- tank_battalion.py
+++ tank_battalion.py
@@ -22,13 +22,24 @@ def draw_player(x,y):
 
 x = (display_width  * 0.45)
 y = (display_height * 0.80)
+x_change = 0
+car_speed = 0
 
 while not crashed:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
 
+    x += x_change
     game_display.fill(white)
     draw_player(x,y)
 
