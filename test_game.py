324b65232e7d15a6ec04a6b8bb15e6c5c0bcd345
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-05 17:58:46 +0200

    Prepare for testing to stay within borders

--- test_game.py
+++ test_game.py
@@ -23,3 +23,13 @@ class TestGame(unittest.TestCase):
         game.add_key(Key.DOWN)
         self.assertEqual(1, len(game.get_keys_pressed()))
 
+    def test_stay_in_screen_when_driving_up(self):
+        game = Game()
+        game.add_key(Key.UP)
+        game.tick()
+        self.assertTrue(is_driving_up(game.get_player()))
+        self.assertTrue(game.get_player().get_y() >= 0.0)
+        for lp in range(game.get_height()):
+            game.tick()
+        #self.assertTrue(game.get_player().get_y() >= 0.0)
+
