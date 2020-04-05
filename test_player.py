0fa94b05f23a75d494c48d3f00854a845e3cfff1
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-05 11:14:24 +0200

    Simplified tests

--- test_player.py
+++ test_player.py
@@ -1,15 +1,10 @@
-# From
-# https://docs.python.org/3/library/unittest.html#basic-example
-
 import unittest
-from player import *
+from player import Player
 
+# Tests the Player class
 class TestPlayer(unittest.TestCase):
 
     def test_upper(self):
         player = Player()
         self.assertEqual(100, player.get_x())
 
-
-if __name__ == '__main__':
-    unittest.main()
